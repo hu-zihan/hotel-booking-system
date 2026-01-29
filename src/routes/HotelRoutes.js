d3ae9cdd7ad60d581d1916060276a50cf55f3806
yyx <codeNoooobYu@outlook.com>
2026-01-29 19:29:05 +0800

    添加数据测试成功

--- src/routes/HotelRoutes.js
+++ src/routes/HotelRoutes.js
@@ -1,9 +1,22 @@
 import express from 'express';
+import { prisma } from '../config/prisma.js';
 const router = express.Router();
 
 // Define your hotel-related routes here
-router.get("list",(res,req)=>{
-    res.send("Hotel list");
+router.get("/list",(req,res)=>{
+    res.json({message: "List of hotels"});
 });
-
+router.post("/add",(req,res)=>{
+    const {name} = req.body;
+    console.log('收到的 name:', name);
+    console.log('完整的请求体:', req.body);
+    prisma.test_table.create({
+        data: { name }
+    }).then((result)=>{
+        console.log("Hotel added:", result);
+        res.json({message: "Hotel added", hotel: result});
+    }).catch((error)=>{
+        res.status(500).json({error: "Failed to add hotel"});
+    });
+})
 export default router;
--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ import {prisma} from './config/prisma.js';
 dotenv.config();
 import hotelRoutes from './routes/HotelRoutes.js';
 const app = express();
+app.use(express.json());
 app.use('/hotels', hotelRoutes);
 app.get("/health", (req, res) => {
     res.setHeader('Content-Type', 'application/json');
@@ -14,11 +15,5 @@ app.get("/health", (req, res) => {
 });
 const port = process.env.PORT;
 const server = app.listen(port, async() => {
-    prisma.test_table.create({data:{name:"test"}}).then(()=>{
-        console.log("Prisma DB connection insert successful!");
-    }).catch((error)=>{
-        console.error("Prisma DB connection failed:", error);
-        process.exit(1); // ❗ 启动即失败，直接退出
-    });
     console.log(`Server is running on port ${port}`);
 })
