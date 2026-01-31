0c575cfd0c407f323fb80578b0c747831275e1e5
yyx <codeNoooobYu@outlook.com>
2026-01-31 16:27:11 +0800

    修改了ad_code表结构增加了详细描述信息,修改了map方法,使得能够直接插入到数据库中

--- mysql/create_table.sql
+++ mysql/create_table.sql
@@ -1,5 +1,6 @@
 create Table IF NOT EXISTS area_adcode_location (
-   adcode VARCHAR(10) PRIMARY KEY,
-   lon VARCHAR(50),
-    lat VARCHAR(50)
+    adcode VARCHAR(10) PRIMARY KEY,
+    lon VARCHAR(50),
+    lat VARCHAR(50),
+    `desc` VARCHAR(255)
 );
