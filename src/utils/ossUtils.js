0fc288b2010adb05507e788acd3f4d39be160052
yyx <codeNoooobYu@outlook.com>
2026-02-17 21:49:07 +0800

    oss upload 函数实现,返回的是url,这个url用来存到hotel_info数据库中

--- src/utils/ossUtils.js
+++ src/utils/ossUtils.js
@@ -15,13 +15,26 @@ const client = new OSS({
   // yourEndpoint填写Bucket所在地域对应的内网Endpoint。以华东1（杭州）为例，Endpoint填写为https://oss-cn-hangzhou-internal.aliyuncs.com。
   endpoint: 'https://oss-cn-hangzhou.aliyuncs.com',
 });
-
-export async function put() {
+async function put(file) {
     try {
         console.log('Starting file upload... current Path is:', process.cwd());
         const result = await client.put('hotels/test.txt', path.join(process.cwd(), 'test.txt'));
         console.log('Upload successful:', result);
     } catch (error) {
         console.error('Error uploading file:', error);
+    }
+}
+export async function upload(file) {
+    try{
+        console.log("starting file upload...");
+        const result = await client.put("hotels/",file);
+        console.log("Upload successful:", result);
+        if(result.status === 200){
+            return result.url;
+        }
+        throw new Error(`Failed to upload file, status code: ${result.status}`);
+    }catch(error){
+         console.error('Error uploading file:', error);
+         throw error;
     }
 }
